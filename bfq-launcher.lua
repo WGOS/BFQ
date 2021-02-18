f9a9981b8e9fdaed613da290c702f9bcc3fd5182
Egor <thewgosplayer@gmail.com>
2021-02-18 15:05:51 +0300

    Add channels

--- bfq-launcher.lua
+++ bfq-launcher.lua
@@ -1,5 +1,6 @@
-local mode, command = ...
-local modes = { "master", "slave" }
+local mode = ...
+local args = { ... }
+local modes = { "master", "slave", "slave-start" }
 
 local function getModemSide()
     for _, side in pairs(rs.getSides()) do
@@ -16,12 +17,37 @@ end
 
 local function runMaster()
     print("Running in master mode")
-    shell.run("wget run https://raw.githubusercontent.com/WGOS/BFQ/master/bfq-master.lua \"" .. command .. "\"")
+
+    if args[2] == nil then
+        print("No command provided")
+        return 3
+    end
+
+    if args[3] == nil then
+        print("No channel provided")
+        return 4
+    end
+
+    shell.run("wget run https://raw.githubusercontent.com/WGOS/BFQ/master/bfq-master.lua \"" .. args[2] .. "\" " .. args[3])
 end
 
 local function runSlave()
     print("Running in slave mode")
-    shell.run("wget run https://raw.githubusercontent.com/WGOS/BFQ/master/bfq-slave.lua")
+
+    if args[3] == nil then
+        print("No channel provided")
+        return 4
+    end
+
+    shell.run("wget run https://raw.githubusercontent.com/WGOS/BFQ/master/bfq-slave.lua " .. args[2])
+end
+
+local function addStartup()
+    print("Adding slave mode file to startup")
+    fs.delete("/startup")
+    local file = fs.open("/startup", "w")
+    file.writeLine("wget run https://raw.githubusercontent.com/WGOS/BFQ/master/bfq-launcher.lua slave " .. args[2])
+    file.close()
 end
 
 local function listModes()
@@ -30,7 +56,6 @@ local function listModes()
         print("\t" .. modes[i])
     end
 end
-
 
 if mode == nil then
     print("No mode selected")
@@ -48,9 +73,11 @@ local modemSide = getModemSide();
 rednet.open(modemSide)
 
 if mode == "master" then
-    runMaster()
+    return runMaster()
 elseif mode == "slave" then
-    runSlave()
+    return runSlave()
+elseif mode == "slave-start" then
+    return addStartup()
 end
 
 rednet.close(modemSide)
--- bfq-slave.lua
+++ bfq-slave.lua
@@ -1,6 +1,9 @@
+local channel = ...
+
 local function listen()
     while true do
-        local senderId, message = rednet.receive("bfq-slave")
+        print("[BFQ] Ready to receive commands at channel" .. channel)
+        local senderId, message = rednet.receive("bfq-slave-" .. channel)
         shell.run("wget run https://raw.githubusercontent.com/WGOS/BFQ/master/quarry.lua " .. message)
     end
 end
